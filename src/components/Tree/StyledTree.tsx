94a7d87bfe90a56cb08baf6dd91ce4567a517fd1
Vazha Dzneladze <vaja.dzneladze@gmail.com>
2023-04-06 15:10:20 +0400

    separated styled Tree

---
+++ src/components/Tree/StyledTree.tsx
@@ -0,0 +1,19 @@
+import { TreeView } from "devextreme-react";
+import styled from "styled-components";
+
+export const StyledTree = styled(TreeView)`
+  /* 
+    background:white;
+    max-height:200px; */
+
+  .dx-treeview-item {
+    /* font-size: 16px;
+    color: #333;
+    padding: 10px;
+    border-bottom: 1px solid #ccc; */
+  }
+
+  .dx-treeview-item-selected {
+    /* background-color: #f5f5f5; */
+  }
+`;
