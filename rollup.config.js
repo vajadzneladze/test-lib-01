b87033181ef2eb82a7016e3dd3ecff9f0e35d16e
Vazha Dzneladze <vaja.dzneladze@gmail.com>
2023-04-09 03:32:09 +0400

    fixed main problem

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,10 @@ import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 import svg from "rollup-plugin-svg";
 import del from "rollup-plugin-delete";
-import url from 'rollup-plugin-url';
-import copy from 'rollup-plugin-copy';
-import path from 'path';
+import url from "rollup-plugin-url";
+import copy from "rollup-plugin-copy";
+import path from "path";
+import rebase from "rollup-plugin-rebase";
 
 const packageJson = require("./package.json");
 
@@ -35,31 +36,34 @@ export default [
     ],
     plugins: [
       del({ targets: "dist/*" }),
-      
+
+      rebase({
+        assets: "src/assets",
+        output: "dist/assets",
+      }),
+
       resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
         alias: {
-          '@': path.resolve(__dirname, 'src'),
+          "@": path.resolve(__dirname, "src"),
         },
       }),
       copy({
-        targets: [
-          { src: 'src/assets', dest: 'dist' },
-        ],
+        targets: [{ src: "src/assets", dest: "dist" }],
       }),
       commonjs(),
       svg({
         transform(svg, id) {
-          console.log('Processing SVG:', id);
+          console.log("Processing SVG:", id);
         },
       }),
       typescript({
-        tsconfig: './tsconfig.json',
+        tsconfig: "./tsconfig.json",
       }),
       url({
         limit: 10 * 1024, // inline files smaller than 10KB
-        include: ['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.eot'], // load only .ttf files
-        emitFiles: true // copy files to output directory
+        include: ["**/*.woff", "**/*.woff2", "**/*.ttf", "**/*.eot"], // load only .ttf files
+        emitFiles: true, // copy files to output directory
       }),
       postcss(),
     ],
