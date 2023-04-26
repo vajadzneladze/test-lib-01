e3dda7c95535220ba2caf85c10c84b6f35aa1156
Vazha Dzneladze <vdzneladze@ip.local>
2023-04-26 18:52:46 +0400

    Story : Form Toggle ,  Toggle : Add Dark / Light / Both version

--- src/stories/FormToggle/FormToggle.stories.tsx
+++ src/stories/FormToggle/FormToggle.stories.tsx
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ThemeProvider, FormToggle } from "../../components";
 
+const withColorScheme = (Story: any, { globals }: any) => {
+
+  let { scheme } = globals;
+
+  return (
+    <div>
+      {
+        scheme === 'light' && <ThemeProvider variant='light'>
+          <div className="bg-for-stories-light">
+            <Story />
+          </div>
+        </ThemeProvider>
+      }
+
+      {
+        scheme === 'dark' && <ThemeProvider variant="dark">
+          <div className="bg-for-stories-dark">
+            <Story />
+          </div>
+        </ThemeProvider>
+      }
+
+      {
+        scheme === 'both' &&
+        <>
+          <ThemeProvider variant="light">
+            <div className="bg-for-stories-light">
+              <Story />
+            </div>
+          </ThemeProvider>
+
+          <ThemeProvider variant="dark">
+            <div className="bg-for-stories-dark">
+              <Story />
+            </div>
+          </ThemeProvider>
+        </>
+      }
+
+    </div>
+  )
+}
+
+
 export default {
   title: "UI-COMPONENTS/FormToggle",
+  decorators: [withColorScheme],
   component: FormToggle,
 } as ComponentMeta<typeof FormToggle>;
 
-const Template: ComponentStory<typeof FormToggle> = (props) => (
-  <ThemeProvider variant={"light"}>
+const Template: ComponentStory<typeof FormToggle> = (props) => {
+
+  return (
     <FormToggle {...props} />
-  </ThemeProvider>
-);
+  );
+}
 
 export const Primary = Template.bind({});
--- src/stories/Toggle/Toggle.stories.tsx
+++ src/stories/Toggle/Toggle.stories.tsx
@@ -2,15 +2,58 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ThemeProvider, Toggle } from "../../components";
 
+
+const withColorScheme = (Story: any, { globals }: any) => {
+
+  let { scheme } = globals;
+
+  return (
+    <div>
+      {
+        scheme === 'light' && <ThemeProvider variant='light'>
+          <div className="bg-for-stories-light">
+            <Story />
+          </div>
+        </ThemeProvider>
+      }
+
+      {
+        scheme === 'dark' && <ThemeProvider variant="dark">
+          <div className="bg-for-stories-dark">
+            <Story />
+          </div>
+        </ThemeProvider>
+      }
+
+      {
+        scheme === 'both' &&
+        <>
+          <ThemeProvider variant="light">
+            <div className="bg-for-stories-light">
+              <Story />
+            </div>
+          </ThemeProvider>
+
+          <ThemeProvider variant="dark">
+            <div className="bg-for-stories-dark">
+              <Story />
+            </div>
+          </ThemeProvider>
+        </>
+      }
+
+    </div>
+  )
+}
+
 export default {
   title: "UI-COMPONENTS/Toggle",
+  decorators: [withColorScheme],
   component: Toggle,
 } as ComponentMeta<typeof Toggle>;
 
 const Template: ComponentStory<typeof Toggle> = (props) => (
-  <ThemeProvider variant={props.isOn ? "light" : "dark"}>
     <Toggle {...props} />
-  </ThemeProvider>
 );
 
 export const Primary = Template.bind({});
